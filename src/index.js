27e3c8b383718e5927aaed878643b7c8ba3e745c
Ndene SENE <ndenenafi10@gmail.com>
2020-08-12 22:41:12 +0000

    First Day

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const Header = (props) => {
+//   return <h1>Hello {props.user}</h1>;
+// };
+
+// function Layout(props) {
+//   return <div style={{ background: "palegoldenrod" }}>
+//     {props.children}
+//   </div>;
+// }
+
+// class Header extends React.Component {
+//   render() {
+//     return <h1>Hello React</h1>;
+//   }
+// }
+
+// function Login() {
+//   return <h1>Please Login!</h1>;
+// }
+
+// function Logout() {
+//   return <button>Se Deconnecter</button>;
+// }
+
+const rootNode = document.getElementById("root");
+
+// const isAuth = true;
+
+function App() {
+  //   return (<Layout>
+  //     {isAuth
+  //       ? (
+  //         <>
+  //           <Header user="Ndene SENE" />
+  //           <Logout />
+  //         </>
+  //       )
+  //       : <Login />}
+  //     {/* {isAuth && <Logout />} */}
+  //     <footer>Ndene SENE 2020</footer>
+  //   </Layout>);
+
+  const people = ["Joe", "ndene", "moussa"];
+  return (
+    <ul>
+      {people.map((person, index) => <Person key={index} person={person} />)}
+      <input onChange={handleInput} />
+    </ul>
+  );
+}
+
+function handleInput(event) {
+  const inputvalue = event.target.value;
+  console.log(inputvalue);
+}
+
+function Person(props) {
+  function handlePersonClick(event) {
+    alert(props.person);
+    console.log(event);
+  }
+  return <li onClick={handlePersonClick}>{props.person}</li>;
+}
+
+ReactDom.render(
+  <App />,
+  rootNode,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
